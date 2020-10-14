cebda7fb49e66635d3c851d692cd4c3f38d87800
RadostinKostadinov <bg.radostin.kostadinov@gmail.com>
2020-10-15 00:32:54 +0300

    test2

--- Ex/ages.js
+++ Ex/ages.js
@@ -11,6 +11,6 @@
             } else if(number >= 66){
                 console.log('elder');
             } else {
-                console.log('out of boundsssssss');
+                console.log('out of boundsssssssss');
             }
         }
