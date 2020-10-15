372063b6ce24df8360e2fe868e0af32104ff35f4
RadostinKostadinov <bg.radostin.kostadinov@gmail.com>
2020-10-15 23:24:08 +0300

    Update palindromeInt.js

--- Ex/palindromeInt.js
+++ Ex/palindromeInt.js
@@ -2,17 +2,37 @@ function palindromeInt(array){
     let myArr = array;
 
     for(let i = 0; i < myArr.length; i++){
-        let palindromeArray = [];
-        let number = myArr[i];
-        let numberToChars = myArr[i];
-        number = number.toString();
-        for(let j = 0; j < number.length; j++){
-            let charN = numberToChars % 10;
-            numberToChars = parseInt(numberToChars/10);
-            palindromeArray.unshift(charN);
-        }
-        console.log(palindromeArray);
+        let palindromeArray = fromNumberToArrayOfChars(myArr[i]);
+        let isPalindrome = isPalindromeF(palindromeArray);
+        console.log(isPalindrome);
     }
 
+
+
+// ============ functions declaration ============
+    function fromNumberToArrayOfChars(number){
+        let n1 = number;
+        let n1toString = number.toString();
+
+        let array = [];
+
+        for(let i = 0; i < n1toString.length; i++){
+            let char = n1 % 10;
+            array.unshift(char);
+            n1 = parseInt(n1 / 10);
+        }
+        
+        return array;
+    }
+
+    function isPalindromeF(array){
+        let isPalindrome = true;
+        for(let i = 0; i < (array.length/2); i++){
+            if(array[i] !== array[(array.length - 1) - i]){
+                isPalindrome = false;
+            }
+        }
+        return isPalindrome;
+    }
 }
-palindromeInt([123,323,421,121]);
+palindromeInt([32,2,232,1010]);
