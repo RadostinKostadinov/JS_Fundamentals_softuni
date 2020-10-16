550e75019c9d5e315f1836996116e80ab3e2c16b
RadostinKostadinov <bg.radostin.kostadinov@gmail.com>
2020-10-16 18:06:36 +0300

    16_Oct_2020

---
+++ Ex/perfectNumber.js
@@ -0,0 +1,11 @@
+function perfectNumber(number){
+    num = Number(num);
+    let arrPosDivisiors = [];
+    for(let i = 1; i <= num; i++){
+        let isPosDivisior = num % i;
+        if(num % )
+    }
+
+}
+
+perfectNumber(6);
