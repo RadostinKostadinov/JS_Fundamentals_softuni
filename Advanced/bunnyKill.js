41cef4cca4678fe33c123246b47e76afee82df6f
RadostinKostadinov <bg.radostin.kostadinov@gmail.com>
2020-10-25 12:20:15 +0200

    asd

--- Advanced/bunnyKill.js
+++ Advanced/bunnyKill.js
@@ -1,9 +1,20 @@
 function bunnyKill(array){
     let myArr = array.slice();
     let arrOfBombsPositions = myArr.splice(myArr.length-1, 1);
-    
-    console.log(myArr);
-    console.log(arrOfBombsPositions);
+    arrOfBombsPositions = arrOfBombsPositions[0].split(' ');     //returns array of BombPos like array[BombNumber][0-> returns row, 2-> column]
+
+    for(let bombNumber = 0; bombNumber < arrOfBombsPositions.length; bombNumber++){
+        let row = arrOfBombsPositions[bombNumber][0];
+        let column = arrOfBombsPositions[bombNumber][2];
+        console.log(`row, column -> ${row}, ${column}`);
+        
+    }
+
+
+    console.log(`arrOfBombsPositions.length -> ${arrOfBombsPositions.length}`);
+    console.log(`arrOfBombsPositions[0][0]  -> ${arrOfBombsPositions[0][2]}`);
+    console.log(`myArr                      -> ${myArr.join('    ')}`);
+    console.log(`arrOfBombsPositions        -> ${arrOfBombsPositions.join(' ')}`);
 
 
 }
@@ -13,5 +24,5 @@ bunnyKill(
     '10 10 10 10',
     '10 15 10 10',
     '10 10 10 10',
-    '2,2 0,1']
+    '2,2 0,1 3,4']
 )
