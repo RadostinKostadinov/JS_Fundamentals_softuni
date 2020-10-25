c60d8e2fe4b49d94714f3085712c34c0fbbc7c78
RadostinKostadinov <bg.radostin.kostadinov@gmail.com>
2020-10-25 13:51:30 +0200

    Update bunnyKill.js

--- Advanced/bunnyKill.js
+++ Advanced/bunnyKill.js
@@ -1,14 +1,33 @@
 function bunnyKill(array) {
     let myArr = array.slice();
     let arrOfBombsPositions = myArr.splice(myArr.length - 1, 1);
-    myArr = arrayTo2DArray(myArr); 
+    myArr = arrayTo2DArray(myArr);
 
     arrOfBombsPositions = arrOfBombsPositions[0].split(' ');     //returns array of BombPos like array[BombNumber][0-> returns row, 2-> column]
 
     for (let bombNumber = 0; bombNumber < arrOfBombsPositions.length; bombNumber++) {
-        let row = arrOfBombsPositions[bombNumber][0];
-        let column = arrOfBombsPositions[bombNumber][2];
+
+        let row = Number(arrOfBombsPositions[bombNumber][0]);
+        let column = Number(arrOfBombsPositions[bombNumber][2]);
         console.log(`row, column -> ${row}, ${column}`);
+        console.log(`           bombValue -> ${myArr[row][column]}`);
+        function boom(array, row, column) {
+            if (row != 0) {
+                if (column - 1 >= 0) array[row - 1][column - 1] -= array[row][column];
+                array[row - 1][column] -= array[row][column];
+                if (column + 1 != array[0].length - 1) array[row - 1][column + 1] -= array[row][column];
+            }
+
+            if (column - 1 >= 0) array[row][column - 1] -= array[row][column];
+            if (column + 1 < array[0].length) array[row][column + 1] -= array[row][column];
+
+            if (row != array.length - 1) {
+                if (column - 1 >= 0) array[row + 1][column - 1] -= array[row][column];
+                array[row + 1][column] -= array[row][column];
+                if (column + 1 < array[0].length) array[row + 1][column + 1] -= array[row][column];
+            }
+            return array;
+        }
 
 
     }
@@ -16,19 +35,19 @@ function bunnyKill(array) {
 
     console.log(`arrOfBombsPositions.length -> ${arrOfBombsPositions.length}`);
     console.log(`arrOfBombsPositions[0][0]  -> ${arrOfBombsPositions[0][2]}`);
-    console.log(`myArr                      -> ${myArr.join('    ')}`);
+    console.log(`myArr                      ->${`\n`}${myArr.join('\n')}`);
     console.log(`arrOfBombsPositions        -> ${arrOfBombsPositions.join(' ')}`);
 
 
-//================= functions declaration =========
-function arrayTo2DArray(array) {
-    for (let row = 0; row < array.length; row++) {
-        let column = array[0].split(' ');
-        array.shift();
-        array.push(column);
+    //================= functions declaration =========
+    function arrayTo2DArray(array) {
+        for (let row = 0; row < array.length; row++) {
+            let column = array[0].split(' ');
+            array.shift();
+            array.push(column);
+        }
+        return array;
     }
-    return array;
-}
 }
 
 bunnyKill(
@@ -36,5 +55,5 @@ bunnyKill(
         '10 10 10 10',
         '10 15 10 10',
         '10 10 10 10',
-        '2,2 0,1 3,4']
+        '2,1 0,3']
 )
