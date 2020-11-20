127794d57bac29d52cfb4984cf609f10b1cced51
RadostinKostadinov <bg.radostin.kostadinov@gmail.com>
2020-11-20 23:08:36 +0200

    Update movies.js

--- Ex/movies.js
+++ Ex/movies.js
@@ -1,68 +1,32 @@
-function movies(array){
-    let myArr = array.slice();
-    let arrayOfMovies = [];
-
-    class Movie {
-        constructor(name){
-            this.name = name;
-        }
-        replaceDate(newDate) {
-            this.date = newDate;
-        }
-
-        replaceDirector(newDirector) {
-            this.director = newDirector;
-        }
-    };
-
-    let action = {
-        addMovie: (name) => {
-            let newMovie = new Movie(name);
-            arrayOfMovies.push(newMovie);
-        },
-        replaceDirector: (name, director) => {
-            for (const movie of arrayOfMovies) {
-                if(movie.name === name){
-                    movie.director = director;   
-                }
+function movies(array) {
+    let movies = [];
+    for (const line of array) {
+        if (line.includes('addMovie')) {
+            let currentMovie = {};
+            let name = line.substring(9);
+            currentMovie.name = name;
+            movies.push(currentMovie);
+        } else if (line.includes('directedBy')) {
+            let commands = line.split(' directedBy ');
+            let [movie, movieDirector] = commands;
+            let myMovie = movies.find(x => x.name == movie);
+            if(myMovie){
+                myMovie.director = movieDirector;
             }
-        },
-        replaceDate: (name, date) => {
-            for (const movie of arrayOfMovies) {
-                if(movie.name === name){
-                    movie.date = date;
-                }
+        } else {
+            let [movie, movieDate] = line.split(' onDate ');
+            let myMovie = movies.find(x => x.name == movie);
+            if(myMovie){
+                myMovie.date = movieDate;
             }
         }
     }
 
-    while(myArr.length > 0){
-        let tokens = myArr.shift().split(' ');
-
-        if(tokens[0] === 'addMovie'){ //addMovie
-            tokens.shift();
-            let name = (tokens.join(' '));
-            action.addMovie(name);
-        } else if(tokens.indexOf('directedBy') != -1){ //directedBy
-            let commandIndex = tokens.indexOf('directedBy');
-            let name = tokens.slice(0, commandIndex).join(' ');
-            let director = tokens.slice(commandIndex+1).join(' ');
-            action.replaceDirector(name, director);
-        } else { //onDate
-            let commandIndex = tokens.indexOf('onDate');
-            let name = tokens.slice(0, commandIndex).join(' ');
-            let date = tokens.slice(commandIndex+1).join(' ');
-            action.replaceDate(name, date);
+    movies.forEach( movie => {
+        if(movie.name && movie.director && movie.date){
+            console.log(JSON.stringify(movie));
         }
-    }
-
-    for (const movie of arrayOfMovies) {
-        let jsonString = JSON.stringify(movie);
-        console.log(jsonString);
-    }
-    
-
-
+    });
 }
 movies([
     'addMovie Fast and Furious',
@@ -73,5 +37,5 @@ movies([
     'Fast and Furious onDate 30.07.2018',
     'Batman onDate 01.08.2018',
     'Fast and Furious directedBy Rob Cohen'
-    ]
-    );
+]
+);
